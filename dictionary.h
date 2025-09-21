c8df35f1cc8cb9612d74dc6b9fa3a2ec8a7d009c
arkame <rhyanclarck123@gmail.com>
2025-09-21 08:49:11 +0300

    d.h

---
+++ dictionary.h
@@ -0,0 +1,21 @@
+#ifndef DICTIONARY_H
+# define DICTIONARY_H
+
+typedef struct s_dict
+{
+	char	*key;
+	char	*value;
+}	t_dict;
+//parcourir le dico
+t_dict	*parse_dict(char *filename);
+//recherche
+char	*dict_lookup(ft_dict *dict, char *key);
+void	free_dict(t_dict *dict);
+// utility
+char	*ft_strdup(char *src);
+int		ft_strlen(char *str);
+int		ft_putstr(char *str);
+void	dict_error(void);
+char	*read_dict_file(char *filename);
+
+#endif
