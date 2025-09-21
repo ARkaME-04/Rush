d4618885bb9059d5cb09ddac4dffd8b77b474ddb
arkame <rhyanclarck123@gmail.com>
2025-09-21 13:36:05 +0300

    d

--- dictionary.h
+++ dictionary.h
@@ -11,6 +11,7 @@ t_dict	*parse_dict(char *filename);
 //recherche
 char	*dict_lookup(ft_dict *dict, char *key);
 void	free_dict(t_dict *dict);
+void	dict_error(void);
 // utility
 char	*ft_strdup(char *src);
 int		ft_strlen(char *str);
